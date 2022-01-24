0e516e1dd2c4070ab32a7f1b8982a239bc63ae2b
LC1A_20_shinkafukumi <k020g1205@g.neec.ac.jp>
2022-01-24 17:51:23 +0900

    とりあえずで分ける場所作成(動く)

---
+++ Game.cpp
@@ -0,0 +1,17 @@
+#include "Game.h"
+
+void Game::Init()
+{
+}
+
+void Game::Finalize()
+{
+}
+
+void Game::Update()
+{
+}
+
+void Game::Draw()
+{
+}
---
+++ Game.h
@@ -0,0 +1,13 @@
+#pragma once
+
+class Game
+{
+public:
+	void Init();
+	void Finalize();
+	void Update();
+	void Draw();
+public:
+private:
+
+};
