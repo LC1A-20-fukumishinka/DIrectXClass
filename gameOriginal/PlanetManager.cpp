909dd3b0306451f3a267c9051bcecbf6898d48d2
LC1A_20_shinkafukumi <k020g1205@g.neec.ac.jp>
2022-06-20 15:32:57 +0900

    惑星管理クラス

---
+++ gameOriginal/PlanetManager.cpp
@@ -0,0 +1,85 @@
+#include "PlanetManager.h"
+#include "../Collision/Collision.h"
+using namespace std;
+using namespace DirectX;
+bool PlanetManager::isMakeInstance = false;
+
+PlanetManager::PlanetManager()
+{
+}
+
+PlanetManager::~PlanetManager()
+{
+}
+
+std::unique_ptr<PlanetManager> &PlanetManager::Instance()
+{
+	static unique_ptr<PlanetManager> instance;
+	if (!isMakeInstance)
+	{
+		PlanetManager *makeInstance = new(PlanetManager);
+		isMakeInstance = true;
+		instance = unique_ptr<PlanetManager>(makeInstance);
+	}
+	return instance;
+}
+
+void PlanetManager::Init()
+{
+}
+
+void PlanetManager::Update()
+{
+	for (auto &e : planets)
+	{
+		e->Update();
+	}
+}
+
+void PlanetManager::Draw()
+{
+	for (auto &e : planets)
+	{
+		e->Draw();
+	}
+}
+
+void PlanetManager::Finalize()
+{
+}
+
+bool PlanetManager::GetGrabPlanet(std::shared_ptr<Planet> &planet, const DirectX::XMFLOAT3 &pos, const DirectX::XMFLOAT3 &angle)
+{
+	float minDist = 10000.0f;
+	bool isCollision = false;
+	Ray cameraRay;
+	cameraRay.dir = XMLoadFloat3(&angle);
+	XMFLOAT3 tmp = pos;
+	cameraRay.start = XMLoadFloat3(&tmp);
+	Sphere starCol;
+	for (auto &e : planets)
+	{
+		starCol.center = XMLoadFloat3(&e->GetPos());
+		starCol.radius = e->GetScale();
+		float colDist;
+		if (Collision::CheckRay2Sphere(cameraRay, starCol, &colDist))
+		{
+			if (colDist <= minDist)
+			{
+				minDist = colDist;
+				planet = e;
+				isCollision = true;
+			}
+		}
+	}
+	return isCollision;
+}
+
+void PlanetManager::AddPlanet(const DirectX::XMFLOAT3 &pos, float size)
+{
+	std::shared_ptr<Planet> planet;
+	planet = make_shared<Planet>();
+	planet->Init(pos, size);
+	int hoge = 0;
+	planets.push_back(planet);
+}
---
+++ gameOriginal/PlanetManager.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "Planet.h"
+#include <list>
+#include <memory>
+class PlanetManager
+{
+private:
+	PlanetManager();
+public:
+	~PlanetManager();
+
+	PlanetManager(PlanetManager &obj) = delete;
+	PlanetManager &operator=(const PlanetManager &obj) = delete;
+
+	static std::unique_ptr<PlanetManager> &Instance();
+
+	void Init();
+	void Update();
+	void Draw();
+	void Finalize();
+public:
+	bool GetGrabPlanet(std::shared_ptr<Planet> &planet, const DirectX::XMFLOAT3 &pos, const DirectX::XMFLOAT3 &angle);
+
+	void AddPlanet(const DirectX::XMFLOAT3 &pos, float size);
+private:
+	std::list<std::shared_ptr<Planet>> planets;
+
+	static bool isMakeInstance;
+};
+
