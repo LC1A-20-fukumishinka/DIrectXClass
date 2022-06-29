d04c3766d38f53f71c1239c8ad1a53dd6739579f
LC1A_20_shinkafukumi <k020g1205@g.neec.ac.jp>
2022-06-30 01:14:07 +0900

    球面上を移動できるように改良のコミットし忘れ

--- gameOriginal/GameCamera.h
+++ gameOriginal/GameCamera.h
@@ -1,23 +1,30 @@
 #pragma once
 #include "../Camera.h"
-
+#include <memory>
+class Planet;
 class GameCamera
 {
 public:
 	void Init();
-	void Update(const Vector3 &playerPos, const Vector3 &playerZVec);
+	void Update(const Vector3 &playerPos, const Vector3 &playerZVec, const Vector3 &playerYVec);
 	void Draw();
 	void Finalize();
 public:
 	Camera *GetCamera();
 	Vector3 GetCameraPos();
+
+	void SetBasePlanet(const std::weak_ptr<Planet> planet);
 private:
 	void NormalUpdate(const Vector3 &playerPos);
-	void LockonUpdate(const Vector3 &playerPos, const Vector3 &playerZVec);
+	void LockonUpdate(const Vector3 &playerPos, const Vector3 &playerZVec, const Vector3 &playerYVec);
 	void GrabUpdate(const Vector3 &playerPos, const Vector3 &playerZVec);
 private:
 	void camRot();
 private:
 	Camera cam;
+
+	Vector3 nextPos = {};
+	DirectX::XMVECTOR nextRot = {};
+	std::weak_ptr<Planet> planet;
 };
 
