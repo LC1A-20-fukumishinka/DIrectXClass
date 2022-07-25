db44b607acc7c080834430cebc9ddb4ba7f34642
LC1A_20_shinkafukumi <k020g1205@g.neec.ac.jp>
2022-07-25 15:43:11 +0900

    レンダーターゲットを追加する際のテクスチャバッファ処理を追加

--- Texture/TextureMgr.cpp
+++ Texture/TextureMgr.cpp
@@ -2,6 +2,8 @@
 #include <cassert>
 #include <DirectXTex.h>
 #include "MyDirectX.h"
+#include "BaseData.h"
+
 TextureMgr::TextureMgr()
 {
 	MyDirectX *myDirectX = MyDirectX::Instance();
@@ -13,7 +15,7 @@ TextureMgr::TextureMgr()
 	descHeapDesc.NumDescriptors = TextureMaxCount;
 	HRESULT result = myDirectX->GetDevice()->CreateDescriptorHeap(
 		&descHeapDesc, IID_PPV_ARGS(&descHeap));
-	LoadTextureCount = 0;
+	loadTextureCount = 0;
 }
 
 TextureMgr::~TextureMgr()
@@ -30,7 +32,7 @@ int TextureMgr::SpriteLoadTexture(const wchar_t *filename)
 {
 	MyDirectX *myDirectX = MyDirectX::Instance();
 	//異常な番号の指定を検出
-	assert(LoadTextureCount <= TextureMaxCount - 1);
+	assert(loadTextureCount <= TextureMaxCount - 1);
 	HRESULT result;
 
 	//WICテクスチャのロード
@@ -59,11 +61,11 @@ int TextureMgr::SpriteLoadTexture(const wchar_t *filename)
 		&texresDesc,
 		D3D12_RESOURCE_STATE_GENERIC_READ,	//テクスチャ用指定
 		nullptr,
-		IID_PPV_ARGS(&texBuff[LoadTextureCount]));
+		IID_PPV_ARGS(&texBuff[loadTextureCount]));
 #pragma endregion
 
 #pragma region texDataSend
-	result = texBuff[LoadTextureCount]->WriteToSubresource(
+	result = texBuff[loadTextureCount]->WriteToSubresource(
 		0,
 		nullptr,							//全領域へコピー
 		img->pixels,							//元データアドレス
@@ -81,21 +83,77 @@ int TextureMgr::SpriteLoadTexture(const wchar_t *filename)
 	srvDesc.Texture2D.MipLevels = 1;
 
 	myDirectX->GetDevice()->CreateShaderResourceView(
-		texBuff[LoadTextureCount].Get(),	//ビューと関連付けるバッファ
+		texBuff[loadTextureCount].Get(),	//ビューと関連付けるバッファ
 		&srvDesc,							//テクスチャ設定情報
 		CD3DX12_CPU_DESCRIPTOR_HANDLE(
-			descHeap->GetCPUDescriptorHandleForHeapStart(), LoadTextureCount,
+			descHeap->GetCPUDescriptorHandleForHeapStart(), loadTextureCount,
 			myDirectX->GetDevice()->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV)
 		)
 	);
 #pragma endregion
 
 	//読み込んだ画像の数を加算する
-	LoadTextureCount++;
+	loadTextureCount++;
 
-	int textureNumber = LoadTextureCount - 1;
+	int textureNumber = loadTextureCount - 1;
 
 	return textureNumber;
+}
+
+void TextureMgr::CreateRenderTarget(std::vector<Microsoft::WRL::ComPtr<ID3D12Resource>> &texBuff, std::vector<int> &textureNums, const int renderTargetCount)
+{
+	HRESULT result;
+
+	for (int i = 0; i < renderTargetCount; i++)
+	{
+		assert(loadTextureCount <= TextureMaxCount - 1);
+#pragma region MakeTextureBuffer
+		//テクスチャリソース設定
+		CD3DX12_RESOURCE_DESC texresDesc = CD3DX12_RESOURCE_DESC::Tex2D(
+			DXGI_FORMAT_R8G8B8A8_UNORM,
+			static_cast<UINT>(WINDOW_WIDTH),
+			static_cast<UINT>(WINDOW_HEIGHT),
+			1, 0, 1, 0, D3D12_RESOURCE_FLAG_ALLOW_RENDER_TARGET
+		);
+
+		const float renderTargetClearColor[4] = { 0.0f,0.0f ,0.0f ,0.0f };
+		//テクスチャバッファの生成
+		result = MyDirectX::Instance()->GetDevice()->CreateCommittedResource(
+			&CD3DX12_HEAP_PROPERTIES(D3D12_CPU_PAGE_PROPERTY_WRITE_BACK,
+				D3D12_MEMORY_POOL_L0),
+			D3D12_HEAP_FLAG_NONE,
+			&texresDesc,
+			D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE,
+			&CD3DX12_CLEAR_VALUE(DXGI_FORMAT_R8G8B8A8_UNORM, renderTargetClearColor),
+			IID_PPV_ARGS(&this->texBuff[loadTextureCount])
+		);
+		assert(SUCCEEDED(result));
+#pragma endregion
+
+#pragma region MakeDescHeap
+
+		//SRV設定
+		D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc{};
+		srvDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+		srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+		srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;//2Dテクスチャ
+		srvDesc.Texture2D.MipLevels = 1;
+
+
+		//デスクリプタヒープにSRVを作成
+		MyDirectX::Instance()->GetDevice()->CreateShaderResourceView(this->texBuff[loadTextureCount].Get(),
+			&srvDesc,
+			CD3DX12_CPU_DESCRIPTOR_HANDLE(descHeap->GetCPUDescriptorHandleForHeapStart(),
+				loadTextureCount,
+				MyDirectX::Instance()->GetDevice()->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV)));
+#pragma endregion
+		//引数のバッファに渡す
+		texBuff.push_back(this->texBuff[loadTextureCount]);
+		textureNums.push_back(loadTextureCount);
+
+		//テクスチャカウントを一つ進める
+		loadTextureCount++;
+	}
 }
 
 ID3D12DescriptorHeap *TextureMgr::GetDescriptorHeap()
@@ -111,9 +169,9 @@ ID3D12Resource *TextureMgr::GetTexBuff(int handle)
 bool TextureMgr::CheckHandle(int handle)
 {
 	bool isCheck = false;
-		if (handle < LoadTextureCount && handle >= 0)
+	if (handle < loadTextureCount && handle >= 0)
 	{
-			isCheck = true;
+		isCheck = true;
 	}
 	return isCheck;
 }
--- Texture/TextureMgr.h
+++ Texture/TextureMgr.h
@@ -19,6 +19,13 @@ public:
 
 	int SpriteLoadTexture(const wchar_t *filename);
 
+	/// <summary>
+	/// レンダーターゲット用のテクスチャバッファの生成を行う
+	/// </summary>
+	/// <param name="texBuff">描画用バッファ</param>
+	/// <param name="textureNums">各テクスチャ番号</param>
+	void CreateRenderTarget(std::vector<Microsoft::WRL::ComPtr<ID3D12Resource>> &texBuff, std::vector<int> &textureNums, const int renderTargetCount);
+
 	ID3D12DescriptorHeap *GetDescriptorHeap();
 
 	ID3D12Resource *GetTexBuff(int handle);
@@ -30,5 +37,5 @@ private:
 	//テクスチャリソース(テクスチャバッファ)の配列
 	Microsoft::WRL::ComPtr<ID3D12Resource> texBuff[TextureMaxCount];
 
-	int LoadTextureCount;
+	int loadTextureCount;
 };
