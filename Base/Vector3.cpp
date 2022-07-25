ce9d9fc21aa59b2034e2ba46d4307a4e433ff7d8
LC1A_20_shinkafukumi <k020g1205@g.neec.ac.jp>
2022-07-25 15:40:51 +0900

    乗算処理追加

--- Base/Vector3.cpp
+++ Base/Vector3.cpp
@@ -24,6 +24,12 @@ const Vector3 operator*(float s, const Vector3 &v)
 	return v * s;
 }
 
+const Vector3 operator*(const Vector3 &v1, const Vector3 &v2)
+{
+	Vector3 temp(v1);
+	return temp *= v2;
+}
+
 const Vector3 operator/(const Vector3 &v, float s)
 {
 	Vector3 temp(v);
@@ -112,6 +118,14 @@ Vector3 &Vector3::operator*=(float s)
 	x = x * s;
 	y = y * s;
 	z = z * s;
+	return *this;
+}
+
+Vector3 &Vector3::operator*=(const Vector3 &v)
+{
+	x = x * v.x;
+	y = y * v.y;
+	z = z * v.z;
 	return *this;
 }
 
