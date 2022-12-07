617fbdc48d4063ea88184bd403f0ea064e710d16
LC1A_20_shinkafukumi <k020g1205@g.neec.ac.jp>
2022-12-07 14:16:21 +0900

    リングゲート実装（要モデル変更）

---
+++ gameOriginal/Gate.cpp
@@ -0,0 +1,97 @@
+#include "Gate.h"
+#include <DirectXMath.h>
+#include "../Collision/Collision.h"
+#include "ModelPhongPipeline.h"
+Model *Gate::sModel = nullptr;
+Camera *Gate::sCamera = nullptr;
+LightGroup *Gate::sLights = nullptr;
+Gate::Gate()
+{
+}
+
+Gate::~Gate()
+{
+}
+
+void Gate::Init(Vector3 pos, Vector3 angle, int index, bool isDraw)
+{
+	pos_ = pos;
+	angle_ = angle;
+	isDraw_ = isDraw;
+	index_ = index;
+	object_.Init();
+	object_.SetCamera(sCamera);
+	object_.SetLightGroup(sLights);
+	object_.SetModel(sModel);
+	Reset();
+}
+
+void Gate::Update()
+{
+}
+
+void Gate::Draw()
+{
+	if (isAlive_)
+	{
+		object_.Update();
+		object_.modelDraw(ModelPhongPipeline::Instance()->GetPipeLine());
+	}
+}
+
+void Gate::Finalize()
+{
+}
+
+void Gate::Reset()
+{
+	object_.SetScale(Vector3(scale_, scale_, 0.1f));
+	object_.SetPosition(pos_);
+	object_.SetRotationVector(DirectX::XMLoadFloat3(&angle_));
+	object_.Update();
+	isAlive_ = true;
+}
+
+void Gate::SetModel(Model *model)
+{
+	sModel = model;
+}
+
+void Gate::SetCamera(Camera *camera)
+{
+	sCamera = camera;
+}
+void Gate::SetLightGroup(LightGroup *lights)
+{
+	sLights = lights;
+}
+
+bool Gate::Collision(Sphere player)
+{
+	if (!isAlive_)
+	{
+		return false;
+	}
+	Sphere gateSphere;
+	gateSphere.center = DirectX::XMLoadFloat3(&pos_);
+	gateSphere.radius = scale_;
+
+	if (Collision::CheckSphere2Sphere(player, gateSphere))
+	{
+		Plane gatePlane;
+		gatePlane.pos = DirectX::XMLoadFloat3(&pos_);
+		gatePlane.normal = DirectX::XMLoadFloat3(&angle_);
+
+		//isAlive_ = false;
+		if (Collision::CheckSphere2Plane(player, gatePlane))
+		{
+			isAlive_ = false;
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	return false;
+}
---
+++ gameOriginal/Gate.h
@@ -0,0 +1,59 @@
+#pragma once
+#include "Object3D.h"
+#include "Vector3.h"
+#include "ModelManager.h"
+#include <memory>
+#include "gameConstData.h"
+#include "../Collision/CollisionPrimitive.h"
+class Gate
+{
+public:
+	Gate();
+	~Gate();
+
+
+	void Init(Vector3 pos, Vector3 angle, int index, bool isDraw);
+	void Update();
+	void Draw();
+	void Finalize();
+
+	void Reset();
+
+	bool Collision(Sphere player);
+
+public:
+	static void SetModel(Model *model);
+	static void SetCamera(Camera *camera);
+	static void SetLightGroup(LightGroup *lights);
+private:
+
+	// 機能している状態か
+	bool isAlive_ = false;
+	//描画しているか
+	bool isDraw_ = true;
+
+	//現在の座標
+	Vector3 pos_;
+
+	//リングの向き
+	Vector3 angle_;
+
+	//半径
+	float scale_ = 30.0f;
+
+
+	//自分のステージ番号
+	int index_ = -1;
+
+	//描画用オブジェクト
+	Object3D object_;
+
+	//モデル
+	static Model *sModel;
+	//カメラ
+	static Camera *sCamera;
+	//ライト
+	static LightGroup *sLights;
+
+};
+
